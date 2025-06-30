32d93fa64992229844275f4f9af3cb67ac5d5518
zixi01.chen <zixi01.chen@horizon.ai>
2025-06-30 06:43:35 +0000

    fix(dnn_node): [no_cr_skip] fix ssd parser error.

--- dnn_node/src/output_parser/detection/ptq_ssd_output_parser.cpp
+++ dnn_node/src/output_parser/detection/ptq_ssd_output_parser.cpp
@@ -114,6 +114,13 @@ int GetBboxAndScores(std::shared_ptr<DNNTensor> cls_tensor,
                      int class_num,
                      float cut_off_threshold);
 
+int GetBboxAndScoresScale(std::shared_ptr<DNNTensor> cls_tensor,
+                     std::shared_ptr<DNNTensor> bbox_tensor,
+                     std::vector<Detection> &dets,
+                     std::vector<Anchor> &anchors,
+                     int class_num,
+                     float cut_off_threshold);
+
 int32_t Parse(
     const std::shared_ptr<hobot::dnn_node::DnnNodeOutput> &node_output,
     std::shared_ptr<DnnParserResult> &result) {
@@ -152,12 +159,21 @@ int PostProcess(std::vector<std::shared_ptr<DNNTensor>> &tensors,
   std::vector<Detection> dets;
   for (int i = 0; i < layer_num; i++) {
     std::vector<Anchor> &anchors = anchors_table_[i];
-    GetBboxAndScores(tensors[i * 2 + 1],
+    if (tensors[0]->properties.quantiType == NONE) {
+      GetBboxAndScores(tensors[i * 2 + 1],
                      tensors[i * 2],
                      dets,
                      anchors,
                      ssd_config_.class_num + 1,
                      0.0001);
+    } else if (tensors[0]->properties.quantiType == SCALE) {
+      GetBboxAndScoresScale(tensors[i * 2 + 1],
+                     tensors[i * 2],
+                     dets,
+                     anchors,
+                     ssd_config_.class_num + 1,
+                     0.0001);
+    }
   }
   nms(dets, nms_threshold_, nms_top_k_, perception.det, false);
   return 0;
@@ -195,6 +211,130 @@ int SsdAnchors(std::vector<Anchor> &anchors,
 }
 
 int GetBboxAndScores(std::shared_ptr<DNNTensor> cls_tensor,
+                     std::shared_ptr<DNNTensor> bbox_tensor,
+                     std::vector<Detection> &dets,
+                     std::vector<Anchor> &anchors,
+                     int class_num,
+                     float cut_off_threshold) {
+  int *shape = cls_tensor->properties.validShape.dimensionSize;
+  int32_t c_batch_size = shape[0];
+  int h_idx, w_idx, c_idx;
+  hobot::dnn_node::output_parser::get_tensor_hwc_index(
+      cls_tensor, &h_idx, &w_idx, &c_idx);
+
+  int32_t c_hnum = shape[h_idx];
+  int32_t c_wnum = shape[w_idx];
+  int32_t c_cnum = shape[c_idx];
+  int32_t anchor_num_per_pixel = c_cnum / class_num;
+
+  shape = bbox_tensor->properties.validShape.dimensionSize;
+  int32_t b_batch_size = shape[0];
+  hobot::dnn_node::output_parser::get_tensor_hwc_index(
+      cls_tensor, &h_idx, &w_idx, &c_idx);
+
+  int32_t b_hnum = shape[h_idx];
+  int32_t b_wnum = shape[w_idx];
+  int32_t b_cnum = shape[c_idx];
+
+  RCLCPP_DEBUG(rclcpp::get_logger("SSDOutputParser"),
+               "PostProcess c_wnum:%d c_hnum:%d c_cnum:%d b_wnum:%d b_hnum:%d "
+               "b_cnum: %d",
+               c_wnum,
+               c_hnum,
+               c_cnum,
+               b_wnum,
+               b_hnum,
+               b_cnum);
+
+  assert(anchor_num_per_pixel == b_cnum / 4);
+  assert(c_batch_size == b_batch_size && c_hnum == b_hnum && c_wnum == b_wnum);
+  auto box_num = b_batch_size * b_hnum * b_wnum * anchor_num_per_pixel;
+
+  hbSysFlushMem(&(cls_tensor->sysMem[0]), HB_SYS_MEM_CACHE_INVALIDATE);
+  auto *raw_cls_data = reinterpret_cast<float *>(cls_tensor->sysMem[0].virAddr);
+
+  hbSysFlushMem(&(bbox_tensor->sysMem[0]), HB_SYS_MEM_CACHE_INVALIDATE);
+  auto *raw_box_data =
+      reinterpret_cast<float *>(bbox_tensor->sysMem[0].virAddr);
+
+  for (int i = 0; i < box_num; i++) {
+    uint32_t res_id_cur_anchor = i * class_num;
+    // get softmax sum
+    double sum = 0;
+    int max_id = 0;
+    // TODO(@d-robotics.cc): fastExp only affect the final score value
+    // confirm whether it affects the accuracy
+    double background_score;
+    if (is_performance_) {
+      background_score = fastExp(raw_cls_data[res_id_cur_anchor]);
+    } else {
+      background_score = std::exp(raw_cls_data[res_id_cur_anchor]);
+    }
+
+    double max_score = 0;
+    for (int cls = 0; cls < class_num; ++cls) {
+      float cls_score;
+      if (is_performance_) {
+        cls_score = fastExp(raw_cls_data[res_id_cur_anchor + cls]);
+      } else {
+        cls_score = std::exp(raw_cls_data[res_id_cur_anchor + cls]);
+      }
+      sum += cls_score;
+      /* scores should be larger than background score, or else will not be
+      selected */
+      if (cls != 0 && cls_score > max_score && cls_score > background_score) {
+        max_id = cls - 1;
+        max_score = cls_score;
+      }
+    }
+    // get softmax score
+    max_score = max_score / sum;
+
+    if (max_score <= score_threshold_) {
+      continue;
+    }
+
+    int start = i * 4;
+    float dx = raw_box_data[start];
+    float dy = raw_box_data[start + 1];
+    float dw = raw_box_data[start + 2];
+    float dh = raw_box_data[start + 3];
+
+    auto x_min = (anchors[i].cx - anchors[i].w / 2);
+    auto y_min = (anchors[i].cy - anchors[i].h / 2);
+    auto x_max = (anchors[i].cx + anchors[i].w / 2);
+    auto y_max = (anchors[i].cy + anchors[i].h / 2);
+
+    auto prior_w = x_max - x_min;
+    auto prior_h = y_max - y_min;
+    auto prior_center_x = (x_max + x_min) / 2;
+    auto prior_center_y = (y_max + y_min) / 2;
+    auto decode_x = ssd_config_.std[0] * dx * prior_w + prior_center_x;
+    auto decode_y = ssd_config_.std[1] * dy * prior_h + prior_center_y;
+    auto decode_w = std::exp(ssd_config_.std[2] * dw) * prior_w;
+    auto decode_h = std::exp(ssd_config_.std[3] * dh) * prior_h;
+
+    auto xmin = (decode_x - decode_w * 0.5);
+    auto ymin = (decode_y - decode_h * 0.5);
+    auto xmax = (decode_x + decode_w * 0.5);
+    auto ymax = (decode_y + decode_h * 0.5);
+
+    xmin = std::max(xmin, 0.0);
+    ymin = std::max(ymin, 0.0);
+
+    if (xmax <= 0 || ymax <= 0) continue;
+    if (xmin > xmax || ymin > ymax) continue;
+
+    Bbox bbox(xmin, ymin, xmax, ymax);
+    dets.emplace_back(static_cast<int>(max_id),
+                      max_score,
+                      bbox,
+                      ssd_config_.class_names[max_id].c_str());
+  }
+  return 0;
+}
+
+int GetBboxAndScoresScale(std::shared_ptr<DNNTensor> cls_tensor,
                      std::shared_ptr<DNNTensor> bbox_tensor,
                      std::vector<Detection> &dets,
                      std::vector<Anchor> &anchors,
